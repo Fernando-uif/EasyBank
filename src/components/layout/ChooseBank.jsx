d8541bf8fc7c39f5ecf10bf233afe22ef2706245
Fernando sosa <fsosat1400@alumno.ipn.mx>
2023-03-13 21:21:41 -0600

    Adjust the choose bank

--- src/components/layout/ChooseBank.jsx
+++ src/components/layout/ChooseBank.jsx
@@ -7,10 +7,11 @@ import easyBank from "../../sass/layout/ChooseBank.module.scss";
 
 export const ChooseBank = () => {
   return (
-    <>
+    <section className={easyBank['card__container']}>
       <section className={`${easyBank['card__title']}`}>
         <h2 className={`${text.subtitle} ${easyBank['card__subtitleCard']}`}>why choose Easybank?</h2>
-        <p className={`${text.detail}  ${easyBank['card__detail']}`}>
+
+        <p className={`${text.detail}  ${easyBank['card__detailDescription']}`}>
           We leverage Open Banking to turn your bank account into your financial
           hub. Control your finances like never before
         </p>
@@ -21,7 +22,7 @@ export const ChooseBank = () => {
             <img src={onlineBanking} />
           </div>
           <h3 className={text.titleCard}>Online Banking</h3>
-          <p className={text.detail}>
+          <p className={`${text.detail} ${easyBank['card__descriptionCard']}`}>
             Our modern web and mobile applications allow you to keep track of
             your finances wherever you are in the world.
           </p>
@@ -31,7 +32,7 @@ export const ChooseBank = () => {
             <img src={budgeting} />
           </div>
           <h3 className={text.titleCard}>Online Banking</h3>
-          <p className={text.detail}>
+          <p className={`${text.detail} ${easyBank['card__descriptionCard']}`}>
             See exactly where your money goes each month. Receive notifications
             when you're close to hitting your limits.
           </p>
@@ -41,7 +42,7 @@ export const ChooseBank = () => {
             <img src={onboarding} />
           </div>
           <h3 className={text.titleCard}>Online Banking</h3>
-          <p className={text.detail}>
+          <p className={`${text.detail} ${easyBank['card__descriptionCard']}`}>
             We don't do branches. Open your account in minutes online and start
             taking control of your finances right away.
           </p>
@@ -51,12 +52,12 @@ export const ChooseBank = () => {
             <img src={api} />
           </div>
           <h3 className={text.titleCard}>Online Banking</h3>
-          <p className={text.detail}>
+          <p className={`${text.detail} ${easyBank['card__descriptionCard']}`}>
             Manage your savings, investments, pension, and much more from one
             account. Tracking your money has never been easier.
           </p>
         </article>
       </section>
-    </>
+    </section>
   );
 };
