1d51aadfea23f41818854215339bc3118e7df983
Fernando sosa <fsosat1400@alumno.ipn.mx>
2023-03-17 22:26:01 -0600

    Add the hover effect on text and buttons

--- src/components/ui/NavBar.jsx
+++ src/components/ui/NavBar.jsx
@@ -2,11 +2,16 @@ import { useEffect, useState } from "react";
 
 import button from "../../sass/components/button.module.scss";
 import logo from "../../assets/images/logo.svg";
-import menuIcon from "../../assets/images/icon-hamburger.svg";
 import navBar from "../../sass/layout/navBar.module.scss";
 
 export const NavBar = () => {
   const [isVisible, setIsVisible] = useState(false);
+  const [changueMenu, setChangueMenu] = useState(false);
+
+  const handleMenu = () => {
+    setChangueMenu(!changueMenu);
+  };
+
   useEffect(() => {
     window.addEventListener("resize", () => {
       window.innerWidth > 768 ? setIsVisible(false) : setIsVisible(true);
@@ -26,12 +31,33 @@ export const NavBar = () => {
 
   return (
     <nav className={navBar.nav}>
+      {isVisible ? (
+        <ul
+          className={`${navBar["nav__menuMobil"]} ${
+            !changueMenu && navBar["nav__menuMobilClose"]
+          }`}
+        >
+          <li>Home</li>
+          <li>About</li>
+          <li>Contact</li>
+          <li>Blog</li>
+          <li>Careers</li>
+        </ul>
+      ) : (
+        <></>
+      )}
+
       <section>
         <img src={logo} alt="logo.svg" />
       </section>
+
       {isVisible ? (
-        <div className={navBar["nav__menu"]}>
-          <img src={menuIcon} alt="icon" />
+        <div className={navBar["nav__menu"]} onClick={handleMenu}>
+          <span
+            className={`${navBar["nav__lines"]} ${
+              changueMenu && navBar["nav__actionLine"]
+            }`}
+          ></span>
         </div>
       ) : (
         <>
